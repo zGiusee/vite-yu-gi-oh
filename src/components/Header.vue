bac0a04a1af58de568c4a264847c8fe59acd1fc0
zGiusee <giusebelforte@gmail.com>
2024-01-10 13:57:02 +0100

    [UPDATE] scaffholding done

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped>
+@use '../styles/generals.scss' as *;
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../styles/generals.scss' as *;
+</style>
