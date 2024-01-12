f92d43bd4a3b35108c2b80e4eef50c153d1a9e5b
zGiusee <giusebelforte@gmail.com>
2024-01-12 10:02:16 +0100

    [UPDATE] Removed search button and added @change

--- src/components/main_components/SearchBox.vue
+++ src/components/main_components/SearchBox.vue
@@ -22,22 +22,23 @@ export default {
             <label for="filter" class="control-label">Archetype</label>
 
             <!-- SELECT CON IL V-MODEL CHE RIMPIE LA VARIABILE VUOTA -->
-            <select class="form-select" id="filter" v-model="store.archetype">
+            <select class="form-select" id="filter" v-model="store.archetype" @change="$emit('sendSearch')">
 
                 <option value=" ">Select Archetype</option>
 
                 <!-- CICLO LE OPTION IN BASE A TUTTI GLI ARCHETIPI DISPONIBILI -->
-                <option v-for="archetype, index in store.archetypes" :key="index" :value="archetype.archetype_name">{{
-                    archetype.archetype_name }}
+                <option v-for="  archetype, index  in  store.archetypes  " :key="index" :value="archetype.archetype_name
+                    ">{{
+        archetype.archetype_name }}
                 </option>
             </select>
 
         </div>
 
-        <div class="col-2">
+        <div class=" col-2">
 
-            <!-- BOTTONE DI RICERCA CON FILTRI -->
-            <button class="btn btn-primary mx-3" @click="$emit('sendSearch')"> Filter </button>
+            <!-- BOTTONE DI RICERCA CON FILTRI CON IL BOTTONE -->
+            <!-- <button class="btn btn-primary mx-3" @click="$emit('sendSearch')"> Filter </button> -->
 
             <!-- BOTTONE DI RESET DELLA RICERCA -->
             <button class="btn btn-danger" @click="resetFilter"> Reset </button>
